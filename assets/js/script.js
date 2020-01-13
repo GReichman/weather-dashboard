b42760b0f20c293c5126c3d1497683dd05a9c880
Garrett Reichman <gsreichman@gmail.com>
2020-01-12 20:36:53 -0500

    added function to create a forecast element

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,3 +7,37 @@ $.ajax({
 }).done(function(response){
     console.log(response);
 });
+
+
+
+var testForecast=createForecastElement("01/12/2020",68.7,60,"sunny");
+
+$("#forecast2").html(testForecast);
+
+
+
+function createForecastElement(date, temp, humidity,weather){   
+    let mediaElement = $("<div>").addClass("media");
+    
+    let mediaPic = $("<img>").addClass("mr-3 weatherPictures");
+    mediaPic.attr({
+        src: "assets/pictures/"+weather+".jpg",
+        alt: weather
+    });
+    mediaElement.append(mediaPic);
+
+
+    let mediaBody = $("<div>").addClass("media-body");
+    let mediaHeader = $("<h5>").addClass("mt-0");
+    mediaHeader.text(date);
+    let mediaTemp = $("<p>").addClass("dayTemp");
+    mediaTemp.text(temp+"F");
+    let mediaHumidity = $("<p>").addClass("dayHumidity");
+    mediaHumidity.text(humidity+"%");
+    mediaBody.append([mediaHeader,mediaTemp,mediaHumidity]);
+
+    mediaElement.append(mediaBody);
+
+    return mediaElement;
+
+}
